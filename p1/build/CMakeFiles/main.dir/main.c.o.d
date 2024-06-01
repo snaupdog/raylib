69b43f20a02d4156422f45d584d1829bcebd083a
snau <nihal.satish@gmail.com>
2024-06-01 16:30:10 +0530

    base setup

---
+++ p1/build/CMakeFiles/main.dir/main.c.o.d
@@ -0,0 +1,14 @@
+CMakeFiles/main.dir/main.c.o: \
+  /Users/nihalsatish/code/raylib_stuff/p1/main.c \
+  /Users/nihalsatish/code/raylib_stuff/p1/include/raylib.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/15.0.0/include/stdarg.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/15.0.0/include/stdbool.h \
+  /Users/nihalsatish/code/raylib_stuff/p1/include/raymath.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/math.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/sys/cdefs.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/sys/_symbol_aliasing.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/sys/_posix_availability.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/Availability.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/AvailabilityVersions.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/AvailabilityInternal.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX14.4.sdk/usr/include/AvailabilityInternalLegacy.h
---
+++ setup/setup.md
@@ -0,0 +1,7 @@
+#How to make the raylib file
+
+* put include/ lib/ in your directory of project
+
+
+cmake -S . -B build
+cmake --build build
